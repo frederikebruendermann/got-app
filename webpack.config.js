af9a5f74e2b2b73c97e227bb48cc0f9f39db1c20
Frederike Bründermann <82148810+frederikebruendermann@users.noreply.github.com>
2021-07-06 16:41:39 +0200

    app webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var path = require('path')
+
+module.exports = {
+  entry: {
+    app: './src/app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'got.bundle.js',
+  },
+}
